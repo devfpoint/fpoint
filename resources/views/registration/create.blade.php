8a47f357341f767bee7d5fc18fc49c3195071ab8
devfpoint <anthonyjezeph@gmail.com>
2016-05-03 16:52:44 +0100

    2

--- resources/views/registration/create.blade.php
+++ resources/views/registration/create.blade.php
@@ -55,9 +55,9 @@
                             <!-- Radio field -->
                            <div class="cc-selector">
                            <input id="visa" type="radio" name="first_name" value="visa" />
-                           <label class="drinkcard-cc visa" for="visa"></label> <br/>Visa
+                           <label class="drinkcard-cc visa" for="visa"></label> 
                            <input id="mastercard" type="radio" name="first_name" value="mastercard" />
-                           <label class="drinkcard-cc mastercard"for="mastercard"></label> <br/>Master
+                           <label class="drinkcard-cc mastercard"for="mastercard"></label> 
                             </div>
    
 
